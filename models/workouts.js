b1539c6970a18fed610f813fbde27c220121c153
Kerri Sexton <k.sexton804@gmail.com>
2021-01-13 17:51:16 -0500

    workout model complete

--- models/workouts.js
+++ models/workouts.js
@@ -14,6 +14,27 @@ const workoutSchema = new Schema({
                 trim: true,
                 required: "Enter exercise type"
             },
+            name: {
+                type: String,
+                trim: true,
+                required: "Enter excercise name"
+            },
+            duration: {
+                type: Number,
+                required: "Enter duration of exercise in minutes"
+            },
+            weight: {
+                type: Number
+            },
+            reps: {
+                type: Number
+            },
+            sets: {
+                type: Number
+            },
+            distance: {
+                type: Number
+            }
         }
     ]
 })
