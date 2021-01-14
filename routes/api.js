165761b79e33fc49b2701bde0ac0f6ea163ccb75
Kerri Sexton <k.sexton804@gmail.com>
2021-01-14 00:02:15 -0500

    not sure what the problem is with the API routes I have

--- routes/api.js
+++ routes/api.js
@@ -1,5 +1,19 @@
 const express = require('express'); 
+const Workout = require('../models/workouts');
 var router = express.Router();
+
+
+module.exports = function(app){
+    app.get('/api/workouts', function(req,res){
+        Workout.find()
+        .then(data => {
+            res.json(data)
+        })
+        .catch(err => {
+            res.json(err)
+        })
+    });
+}
 
 
 // /api/workouts
